0e2ba1c7f9d3eee2053dbab24e49d818b657dcb7
dfn-slxxp <seb@sebastianw.tech>
2025-02-06 01:56:16 -0500

    fix shiz

--- fr-caucus-site/twentyTwentyfive/views.py
+++ fr-caucus-site/twentyTwentyfive/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 from django.template import loader
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base-header-footer.html')
